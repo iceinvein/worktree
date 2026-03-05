42f251c9938751dfecd0c58ccc10c1dc82c6e10a
Dik Rana <dik.rana@mechanicalrock.io>
2026-03-05 16:08:09 +0800

    feat: show ahead/behind badges on branch items

--- src/branchProvider.ts
+++ src/branchProvider.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import type { Branch, GitService } from "./gitService";
+import { formatBranchDescription } from "./utils/worktreeHelpers";
 
 export class BranchProvider implements vscode.TreeDataProvider<BranchItem> {
 	private _onDidChange = new vscode.EventEmitter<void>();
@@ -18,19 +19,51 @@ export class BranchProvider implements vscode.TreeDataProvider<BranchItem> {
 	async getChildren(): Promise<BranchItem[]> {
 		const config = vscode.workspace.getConfiguration("worktreeManager");
 		const showRemote = config.get<boolean>("showRemoteBranches", true);
+		const baseBranch = config.get<string>("baseBranch", "main");
 		const branches = await this.git.getBranches(showRemote);
-		return branches.map((b) => new BranchItem(b));
+
+		const enriched = await Promise.all(
+			branches.map(async (b) => {
+				const localName = b.name.replace(/^origin\//, "");
+				const [aheadBehind, behindRemote] = await Promise.all([
+					this.git.getBranchAheadBehind(
+						b.isRemote ? b.name : localName,
+						baseBranch,
+					),
+					b.isRemote
+						? Promise.resolve(0)
+						: this.git.getBranchBehindRemote(localName),
+				]);
+				return {
+					...b,
+					ahead: aheadBehind.ahead,
+					behind: aheadBehind.behind,
+					behindRemote,
+				};
+			}),
+		);
+
+		return enriched.map((b) => new BranchItem(b, baseBranch));
 	}
 }
 
 export class BranchItem extends vscode.TreeItem {
-	constructor(public readonly branch: Branch) {
+	constructor(
+		public readonly branch: Branch,
+		baseBranch: string,
+	) {
 		super(branch.name, vscode.TreeItemCollapsibleState.None);
 
 		this.iconPath = new vscode.ThemeIcon(
 			branch.isRemote ? "cloud" : "git-branch",
 		);
-		this.description = branch.isRemote ? "remote" : "";
+		this.description = formatBranchDescription({
+			isRemote: branch.isRemote,
+			ahead: branch.ahead ?? 0,
+			behind: branch.behind ?? 0,
+			behindRemote: branch.behindRemote ?? 0,
+			baseBranchName: baseBranch,
+		});
 		this.contextValue = "branch";
 
 		this.command = {
